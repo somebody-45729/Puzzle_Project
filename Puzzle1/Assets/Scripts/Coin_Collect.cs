3790d2d755cc86a67722fc6dc09b288d7b6f706b
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-03-02 19:34:56 -0600

    update

--- Puzzle1/Assets/Scripts/Coin_Collect.cs
+++ Puzzle1/Assets/Scripts/Coin_Collect.cs
@@ -6,7 +6,7 @@ public class Coin_Collect : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player_Char")
+        if (other.tag == "Player")
         {
             Debug.Log("Coin Get!");
             Destroy(this.gameObject);
